ea6495de664ce33b687ac9c74e4e2fe62e307c82
Jun-0820 <woksw.woksw@gmail.com>
2025-11-05 17:33:22 +0900

    Create MyMouseListener.java

---
+++ src/main/java/com/skhu/tips/controller/MyMouseListener.java
@@ -0,0 +1,17 @@
+package com.skhu.tips.controller;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+public class MyMouseListener implements MouseListener {
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+}
+
+
+
