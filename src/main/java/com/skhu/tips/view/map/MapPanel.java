a190c6b906b533cfe487a2a5bbd0d402712a6efe
Jun-0820 <woksw.woksw@gmail.com>
2025-11-05 17:31:37 +0900

    Update MapPanel.java
    맵 패널 생성 및 그리기(패널의 좌표값, 사이즈 등의 속성이 여기에 저장됨)

--- src/main/java/com/skhu/tips/view/map/MapPanel.java
+++ src/main/java/com/skhu/tips/view/map/MapPanel.java
@@ -1,5 +1,13 @@
 package com.skhu.tips.view.map;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
 /*
  * ***************************
  *  파일명: MapPanel
@@ -9,6 +17,48 @@ package com.skhu.tips.view.map;
  * ***************************
  */
 
-public class MapPanel {
+public class MapPanel extends JPanel {
+	private ImageIcon icon = new ImageIcon("src/image/testImage.jpg");	//배경
+	private Image img = icon.getImage();
+	
+	
+	private int width = 500, height = 500;	//맵 가로, 세로 길이. 기본값 500x500, 언제든 수정가능
+	private int x = 0, y = 0;		//이미지의 첫 좌표, 수정가능
+	private int current_x, current_y;	//마우스 클릭 시의 커서 좌표
+	
+	
+	public void setImg(String imagePath) {
+		this.icon = new ImageIcon(imagePath);
+		this.img = icon.getImage();
+		repaint();
+	}
+	
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setCurrentXY(int x, int y) {
+		this.current_x = x;
+		this.current_y = y;
+	}
+	
+	public void setImgSize(int width, int height) {
+		this.width = width;
+		this.height = height;
+		repaint();
+	}
+	
+	public int getImgX() {	return this.x; }
+	public int getImgY() { return this.y; }
+	public int getCurrent_X() { return this.current_x; }
+	public int getCurrent_Y() { return this.current_y; }
+	public int getImgW() { return this.width; }
+	public int getImgH() { return this.height; }
+	
+	public void paintComponent(Graphics g) {	//지도 그리기
+		super.paintComponent(g);
+		g.drawImage(img, x, y, width, height, this);	//지도 배경 그리기
 
+	}
 }
